36691d5e87d8ccdaf4c27b484cb3b08d2f2cac5f
Divyanshu Parihar <divyanshu1447@gmail.com>
2022-01-19 06:21:23 +0000

    checking

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ int main() {
 
     // <----Write a C function to return minimum and maximum in an array.
     //  Your program should make the minimum number of comparisons. --->
-    int arr[] = {5 , 4, 3, 2, 1};
-    int* ans = maxandMin(arr, 5);
-    printArr(ans, 2);
+    // int arr[] = {5 , 4, 3, 2, 1};
+    // int* ans = maxandMin(arr, 5);
+    // printArr(ans, 2);
 } 
