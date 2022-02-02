bcebb2286cfd0866a6b420dfa3f7cc0752959309
divyanshu-parihar <divyanshu1447@gmail.com>
2022-02-02 12:27:27 +0530

    updating file structure

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
-#include "./allheaders/all.h"
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
-    
+    int arr[] = {1,2,3,8};
+    // getSumSubarray(arr,4,5);
+//    printArr(result,2);
 } 
